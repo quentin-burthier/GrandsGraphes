e5dcac41865c0638a61b9c18bea8da75b67f3a00
conservation-decisions <45868959+conservation-decisions@users.noreply.github.com>
2020-04-08 13:52:00 +0200

    label propagation

--- src/TP3/community_detection.jl
+++ src/TP3/community_detection.jl
@@ -3,34 +3,16 @@ using Random
 using GraphPlot
 import Cairo, Fontconfig
 using Compose
+using Colors
 
-include("label_propagation.jl")
-using .LabelPropagation: labelpropagation
-
+#exercice 1
 function plot_graphs(nb_nodes=400, nb_clusters=4, q=0.1)
     p = [0.1, 0.3, 0.5, 0.9]
     for p_i in p
         g, y = generate_graph(p_i, q, nb_nodes, nb_clusters)
-        draw(PNG("../res/$(p_i)_propagation.png", 16cm, 16cm), gplot(g))
+        draw(PNG(string("../res/", p_i, "_clusters.png"), 16cm, 16cm), gplot(g))
     end
 end
-
-
-function main_label_propagation(nb_nodes=400, nb_clusters=4, q=0.1)
-    p = [0.1, 0.3, 0.5, 0.9]
-    for p_i in p
-        graph, gold_labels = generate_graph(p_i, q, nb_nodes, nb_clusters)
-        
-        labels = labelpropagation(graph)
-        # labels, _ = label_propagation(graph) # LightGraph
-        println(collect(zip(labels, gold_labels)))
-        # nodecolor = [colorant"lightseagreen", colorant"orange", colorant"red", colorant"blue"]
-        # nodefillc = nodecolor[membership]
-        # draw(PNG("../res/$(p_i)_propagation.png", 16cm, 16cm), 
-        #      gplot(graph, nodefillc=nodefillc))
-    end
-end
-
 
 function generate_graph(p::Float64, q::Float64, nb_nodes::Int64, nb_clusters::Int64)
     clusters = Vector{Int64}()
@@ -58,10 +40,110 @@ function generate_graph(p::Float64, q::Float64, nb_nodes::Int64, nb_clusters::In
     for edge in edges
         add_edge!(g, edge[1], edge[2])
     end
-
     return g, clusters
 end
 
 
 # plot_graphs()
+
+#exercice 2
+
+function fy_shuffle!(tab) 
+    #Fisher-Yates shuffle
+    list_range = [1:1:length(tab);]
+    for i in list_range
+        j = rand(list_range[1]: list_range[end])
+        tab[i], tab[j] = tab[j], tab[i]
+    end
+    return(tab)
+end
+
+function neighbour_label(graph, node, labels)
+    #find label occurring with the highest frequency among neighbours of node in graph
+    unique_labels = unique(labels)
+    values = zeros(length(unique_labels))
+    scores = Dict(zip(unique_labels, values))
+    
+    for neighb in  neighbors(graph, node)
+        neighb_label = labels[neighb]
+        scores[neighb_label] += 1
+    end
+    a = argmax(scores)
+
+    return a
+end
+
+function label_propagation_step_three(graph,labels)
+
+    new_labels =zeros(length(labels))
+    graph_nodes = [1:1:nv(graph);]
+    shuffled_nodes = fy_shuffle!(graph_nodes)
+
+    #for each node in the network (in this random order)
+    # set its label to a label occurring with the highest frequency among its neighbours
+    for node in shuffled_nodes
+        new_labels[node] = neighbour_label(graph,node,labels)
+    end
+    
+    return new_labels
+end
+
+function label_propagation!(graph, init_labels, max_iter=1000)
+
+    test = true #bool that indicates if the we keep iterating the algorithm
+    n_iter = 0
+    labels = init_labels
+    
+    while test
+        #step three: label propagation
+        labels = label_propagation_step_three(graph,labels)
+        
+        #stoping criteria
+        test = false
+        for node in vertices(graph)
+            # Check if one node doesn't belong to the same communities than the majority of its neighbors
+            if labels[node] != neighbour_label(graph,node,labels)
+                test = true# If one node doesn't satisfy the condition, we need to continue
+            end
+        end
+        
+        if n_iter > max_iter
+            test = false
+        end
+        
+        n_iter+=1
+    end
+    
+    return labels
+end
+                
+
+function main_label_propagation(nb_nodes=400, nb_clusters=4, q=0.1)
+    p = [0.1, 0.3, 0.5, 0.9]
+#     p = [0.1]
+    for p_i in p
+        graph, true_y = generate_graph(p_i, q, nb_nodes, nb_clusters)
+        
+        ##
+        init_y = [1:1:nv(graph);]
+        init_y = fy_shuffle!(init_y)
+        
+        label_prop_y = label_propagation!(graph, init_y)
+        membership = unique(label_prop_y)
+        chosen=[]
+        for l in label_prop_y
+            f = findall(membership .== l)
+            push!(chosen, f[1])
+        end
+        
+        nodecolor = [colorant"lightseagreen", colorant"orange", colorant"blue", colorant"purple"]
+        nodefillc = nodecolor[chosen]
+        draw(PNG(string("../res/", p_i, "_propagation.png"), 16cm, 16cm),gplot(graph, nodefillc=nodefillc))
+
+	nodefillc2 = nodecolor[true_y]
+	draw(PNG(string("../res/", p_i, "_true.png"), 16cm, 16cm),gplot(graph, nodefillc=nodefillc2))
+    end
+end
+
+
 main_label_propagation()
